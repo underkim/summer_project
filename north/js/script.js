7f521c98d7ef4682a3d55800263a07111f09a6fe
glaxy3 <rlaehdgns4568@gmail.com>
2023-07-09 14:34:54 +0900

    north test case 1

--- north/js/script.js
+++ north/js/script.js
@@ -1,40 +1,32 @@
-import data from "../data.js"
 
 
-function left_img() {
-
-    const first = document.getElementById("first");
-    const second = document.getElementById("second");
-
-    first.style.visibility = "none";
-    second.style.visibility = "hidden";
-}
-function right_img() {
-
-    let first = document.getElementById("first");
-    let second = document.getElementById("second");
-
-    second.innerHTML = first.innerHTML;
-    first.innerHTML = '<div class="col"</div>';
-}
-
-// 
-window.onload = function () {
-    var el = document.getElementById("choice");
-    el.onclick = left_img;
-}
-
-function write_list()
+var test =
 {
-const li = document.createElement("li");
-li.setAttribute('id', data.test.name);
+    name: "fk",
+    location: "loc",
+    parking: "par",
+    picture: "pic",
+    explain: "exp",
 
-const textNode = document.createTextNode(data.test.name);
-li.appendChild(textNode);
-
-
-document
-    .getElementById('listUp')
-    .appendChild(li);
 }
 
+window.onload = function write_list() {
+
+    let text = "<ul list-style:none;>";
+
+
+    text += "<li>" + test.name + "</li>";
+
+    text += "</ul>";
+
+    document.getElementById("list").innerHTML = text;
+}
+
+function change_img() {
+    let front = document.getElementById("div_img1");
+    front.style.visibility = 'visible';
+    let second = document.getElementById("div_img2");
+    second.style.visibility = 'hidden';
+    let list = document.getElementById("list");
+    list.style.visibility = 'visible';
+}
