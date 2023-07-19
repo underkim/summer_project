1ac5511084c043e6de48b5741805e51cdcfa6554
okayeyo <clskfk1234@naver.com>
2023-07-19 14:43:09 +0900

    중심좌표 js

---
+++ mapjs/mapjs.js
@@ -0,0 +1,59 @@
+//광주광역시 중심
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div 
+    staticMapOption = {
+        center: new kakao.maps.LatLng(35.1634932, 126.8581248), // 이미지 지도의 중심좌표
+        level: 9.5// 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
+//광산구 중심
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div  
+    staticMapOption = {
+        center: new kakao.maps.LatLng(35.1651523, 126.752968), // 이미지 지도의 중심좌표
+        level: 9// 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
+//북구 중심
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div 
+    staticMapOption = {
+        center: new kakao.maps.LatLng(35.1970887, 126.9265319), // 이미지 지도의 중심좌표
+        level: 9// 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
+//서구 중심
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div 
+    staticMapOption = {
+        center: new kakao.maps.LatLng(35.1414635, 126.856259), // 이미지 지도의 중심좌표
+        level: 9// 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
+//남구 중심
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div 
+    staticMapOption = {
+        center: new kakao.maps.LatLng(35.0928209, 126.8529932), // 이미지 지도의 중심좌표
+        level: 9// 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
+
+//동구 중심
+var staticMapContainer = document.getElementById('staticMap'), // 이미지 지도를 표시할 div 
+    staticMapOption = {
+        center: new kakao.maps.LatLng(35.1206, 126.9527), // 이미지 지도의 중심좌표
+        level: 8.5// 이미지 지도의 확대 레벨
+    };
+
+// 이미지 지도를 표시할 div와 옵션으로 이미지 지도를 생성합니다
+var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
